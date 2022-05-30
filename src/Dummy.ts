586ca772ec452a5ab8acba75b06897dc48b1787b
Saurabh Mahajan <saurabhmahajan@Apples-MacBook-Pro-5.local>
2022-05-30 16:02:42 +0530

    Created 3 testcases to check for prime Factorization

--- src/Dummy.ts
+++ src/Dummy.ts
@@ -1,3 +1,24 @@
-export const dummyOutput = (input: string) => {
-    return input + input
+export const primeFactorization = (n : number) => {
+
+       var primeFactors: number[] = [];
+       if(n==0){
+           return primeFactors
+       }
+       while(n%2==0){
+            primeFactors.push(2)
+            n=n/2
+       }
+       for (var i = 3; i <= Math.sqrt(n); i = i + 2)
+       {
+               while (n % i == 0)
+               {
+                   primeFactors.push(i)
+                   n = n/i;
+               }
+       }
+
+       if (n > 2){
+            primeFactors.push(n)
+       }
+       return primeFactors
 }
--- test/src/Dummy.test.ts
+++ test/src/Dummy.test.ts
@@ -1,8 +1,23 @@
-import { dummyOutput } from '../../src/Dummy'
+
+import {primeFactorization} from '../../src/Dummy'
 
 describe("Dummy Test", () => {
-    it("should assert on the dummy output", () => {
-        const dummyOutputReturned = dummyOutput("hello")
-        expect(dummyOutputReturned).toBe("hellohello")
+
+    it("check for prime factorization of 315, we should get output as [3, 3, 5, 7]", () => {
+        const gettingValue = primeFactorization(315)
+        expect(gettingValue).toEqual([3, 3, 5, 7])
     })
+
+    it("check for prime factorization of 0, we should get output as 0", () => {
+        const gettingValue = primeFactorization(0)
+        expect(gettingValue).toEqual([])
+    })
+    it("check for prime factorization of 99, we should get output as 0", () => {
+        const gettingValue = primeFactorization(99)
+        expect(gettingValue).toEqual([3,3,11])
+    })
+
 })
+
+
+
